45a1c0027d90ffb2c55147699dd2214e9fb56af3
AxelQw909 <dart58215@gmail.com>
2025-12-25 22:14:06 +0500

    Работа с middleware

---
+++ app/Http/Middleware/UserMiddleware.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserMiddleware
+{
+    public function handle(Request $request, Closure $next): Respose{
+        if (!auth()->check() || auth()->user()->isAdmin()){
+            abort(403, 'Доступ запрещен');
+        }
+        return $next($request);
+    }
+}
